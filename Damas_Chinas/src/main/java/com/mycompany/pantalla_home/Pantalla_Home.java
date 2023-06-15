7f919c83a17275ae8a2c0154ea1a9397a83ae3bd
Lucho-rar <lucianorodriguez131@mi.unc.edu.ar>
2023-06-15 16:20:40 -0300

    primercommit

---
+++ Damas_Chinas/src/main/java/com/mycompany/pantalla_home/Pantalla_Home.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pantalla_home;
+
+/**
+ *
+ * @author lucho
+ */
+public class Pantalla_Home {
+
+    public static void main(String[] args) {
+        Vista_Home vistaHome = new Vista_Home();
+        
+                
+        vistaHome.setVisible(true);
+        
+    }
+}
