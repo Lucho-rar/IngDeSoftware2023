176ffc6d5d2920776453d6c5b9aaa4c62376fb7c
Lucho-rar <lucianorodriguez131@mi.unc.edu.ar>
2023-06-17 18:26:54 -0300

    ImplementacionSingleton

--- Damas_Chinas/src/main/java/com/mycompany/pantalla_home/Singleton.java
+++ Damas_Chinas/src/main/java/com/mycompany/pantalla_home/Singleton.java
@@ -1,19 +1,16 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.pantalla_home;
-
-
 
 /**
  *
  * @author lucho
  */
-public class Pantalla_Home {
-    
-    public static void main(String[] args) {
-        /*
+public class Singleton {
+    private static Singleton uniqueInstance;
+    private Singleton() {
         Music_Nav m = new Music_Nav();
         m.performMusic();
         Vista_Config config = new Vista_Config();
@@ -22,9 +19,13 @@ public class Pantalla_Home {
         Vista_Home vistaHome = new Vista_Home(m,config);                
         vistaHome.setLocationRelativeTo(null);
         vistaHome.setVisible(true);
-        */
-        Singleton singleton = Singleton.getInstance();
-//this.setLocationRelativeTo(null);
-        
     }
+    
+    public static Singleton getInstance(){
+        if(uniqueInstance ==null){
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+    
 }
